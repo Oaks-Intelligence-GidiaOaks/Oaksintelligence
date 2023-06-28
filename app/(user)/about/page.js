bff30bdf543d8b7319d7326a1c411000259795b5
JSjags <jesseabuaja@hotmail.com>
2023-06-28 12:59:36 +0100

    Test deployment

--- app/(user)/about/page.js
+++ app/(user)/about/page.js
@@ -6,9 +6,19 @@ import Hero from "@/components/about/Hero";
 import Integration from "@/components/about/Integration";
 import Services from "@/components/home/Services";
 import Solutions from "@/components/about/Solutions";
-import React from "react";
+import React, { useContext, useLayoutEffect } from "react";
+import AppContext from "@/contexts/AppContext";
 
 const About = () => {
+  const { setTheme } = useContext(AppContext);
+
+  useLayoutEffect(() => {
+    const cachedTheme = localStorage.getItem("oi-theme")
+      ? localStorage.getItem("oi-theme")
+      : "light";
+    setTheme(cachedTheme);
+  }, [setTheme]);
+
   return (
     <main className="min-h-screen bg-white dark:bg-main overflow-hidden transition-[background] duration-500 ease-in-out">
       <Hero />
--- app/(user)/contact/page.js
+++ app/(user)/contact/page.js
@@ -1,10 +1,19 @@
 "use client";
-import React from "react";
+import React, { useContext, useLayoutEffect } from "react";
 import Hero from "@/components/contact/Hero";
 import FirstSection from "@/components/contact/FirstSection";
 import Remark from "@/components/contact/Remark";
+import AppContext from "@/contexts/AppContext";
 
 const About = () => {
+  const { setTheme } = useContext(AppContext);
+
+  useLayoutEffect(() => {
+    const cachedTheme = localStorage.getItem("oi-theme")
+      ? localStorage.getItem("oi-theme")
+      : "light";
+    setTheme(cachedTheme);
+  }, [setTheme]);
   return (
     <main className="min-h-screen bg-white dark:bg-main overflow-hidden transition-[background] duration-500 ease-in-out">
       <Hero />
