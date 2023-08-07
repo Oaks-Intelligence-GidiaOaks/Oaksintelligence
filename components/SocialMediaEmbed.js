cfa260452338e69d3241dd80d9ca8e3490be6dd5
JSjags <jesseabuaja@hotmail.com>
2023-08-07 06:20:08 +0100

    Replaced all social media widgets in website footer.

--- components/SocialMediaEmbed.js
+++ components/SocialMediaEmbed.js
@@ -62,15 +62,26 @@ const SocialMediaEmbed = () => {
       <div className="relative w-full my-10">
         <div className="w-full rounded-[10px] overflow-visible">
           <div className="flex relative z-10 flex-nowrap gap-6 px-[5%] py-6 overflow-scroll hide-scrollbar">
+            {/* Linkedin */}
             <div className="max-h-[372px] min-w-[325px] drop-shadow-[0_5px_7px_#00000035] inner-scrollbar overflow-hidden rounded bg-white overflow-y-scroll font-['Poppins,sans-serif_!important'] scrollbar-hidden">
-              <LinkedInEmbed
-                postUrl="https://www.linkedin.com/posts/oaks-intelligence-limited_ecoweather-ecotracker-nigeria-activity-7085600487823032320-1eSK?utm_source=share&utm_medium=member_desktop"
+              {/* <LinkedInEmbed
+                // postUrl="https://www.linkedin.com/posts/oaks-intelligence-limited_ecoweather-ecotracker-nigeria-activity-7085600487823032320-1eSK?utm_source=share&utm_medium=member_desktop"
                 url="https://www.linkedin.com/embed/feed/update/urn:li:share:7085600486321512448"
                 width={325}
-              />
+              /> */}
+              <div
+                class="sk-ww-linkedin-page-post"
+                data-embed-id="176669"
+              ></div>
+              <script
+                src="https://widgets.sociablekit.com/linkedin-page-posts/widget.js"
+                async
+                defer
+              ></script>
             </div>
-            <div className="max-h-[372px] px-2 min-w-[325px] drop-shadow-[0_5px_7px_#00000035] inner-scrollbar overflow-hidden rounded bg-white overflow-y-scroll scrollbar-hidden">
-              <Tweet
+            {/* X */}
+            <div className="max-h-[372px] min-w-[325px] drop-shadow-[0_5px_7px_#00000035] inner-scrollbar overflow-hidden bg-white rounded-xl overflow-y-scroll scrollbar-hidden">
+              {/* <Tweet
                 options={{
                   chrome: "noborders transparent",
                 }}
@@ -78,21 +89,49 @@ const SocialMediaEmbed = () => {
                   <p className="poppins-4 p-3">Could not load tweet</p>
                 )}
                 tweetId="1679608885210906624"
-              />
+              /> */}
+              {/* <Timeline
+                dataSource={{ sourceType: "profile", screenName: "Oaksintel" }}
+                options={{
+                  width: "320",
+                  height: "372",
+                  // dnt: false,
+                  borderColor: "#FF0000",
+                }}
+              /> */}
+              <a
+                class="twitter-timeline"
+                data-width="325"
+                data-height="372"
+                href="https://twitter.com/Oaksintel?ref_src=twsrc%5Etfw"
+              ></a>
+              <script
+                async
+                src="https://platform.twitter.com/widgets.js"
+                charset="utf-8"
+              ></script>
             </div>
+            {/* Facebook */}
             <div className="max-h-[372px] min-w-[325px] drop-shadow-[0_5px_7px_#00000035] inner-scrollbar overflow-hidden bg-white rounded overflow-y-scroll scrollbar-hidden">
-              <FacebookEmbed
-                placeholderSpinnerDisabled={true}
-                url="https://web.facebook.com/oaksintel/posts/pfbid0cvUfLSAtKdyCY4DKoZhctBcMWhc5kD13tAf2Vdcn8wKCLgEEjD55UajCnDfsQ8ynl?__cft__[0]=AZXWdxXO6LjFKbvvrJI-wA_ibYtqbYxS6AJeaeOH34ryVM2DhS8EzqtVHU6RaVJvzW1omxxSgV_PgjRs9y_c_fNfQ-vREE7m2imnOTHZWA75cX8TQJx6Y6ew05K7s3U7qv1j8kEAka-CRkCwM4Ulc8e9ygh6LG9elspdUkchHL1Aws9ZVgZ0_rLYxuCiUfYWIF8&__tn__=%2CO%2CP-R"
-                width={325}
-              />
+              <iframe
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fweb.facebook.com%2Foaksintel&tabs=timeline&width=325&height=372&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+                width="325"
+                height="372"
+                style={{ border: "none", overflow: "hidden" }}
+                scrolling="no"
+                frameborder="0"
+                allowfullscreen="true"
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+              ></iframe>
             </div>
+            {/* Instagram */}
             <div className="max-h-[372px] min-w-[328px] drop-shadow-[0_5px_7px_#00000035] inner-scrollbar overflow-hidden rounded bg-white overflow-y-scroll scrollbar-hidden">
-              <InstagramEmbed
-                igVersion=""
-                url="https://www.instagram.com/p/CurafBaoVOb/"
-                width={328}
-              />
+              <div class="sk-instagram-feed" data-embed-id="176661"></div>
+              <script
+                src="https://widgets.sociablekit.com/instagram-feed/widget.js"
+                async
+                defer
+              ></script>
             </div>
             {/* Youtube */}
             {/* <div className="max-h-[372px] min-w-[325px] drop-shadow-[0_5px_7px_#00000035] overflow-hidden rounded bg-white overflow-y-scroll">
