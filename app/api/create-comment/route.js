ffa0739ae08f9b63ec29ce6f482b064d8c268c6b
JSjags <jesseabuaja@hotmail.com>
2023-11-18 16:04:48 +0100

    Fix: handled error catching in comment api route.

--- app/api/create-comment/route.js
+++ app/api/create-comment/route.js
@@ -10,21 +10,27 @@ import {
 export async function POST(request) {
   const { name, text, _id } = await request.json();
 
-  client
-    .config({
-      token: apiToken,
-    })
-    .create({
-      _type: "comment",
-      name,
-      text,
-      post: {
-        _type: "reference",
-        _ref: _id,
-      },
-    });
+  try {
+    client
+      .config({
+        token: apiToken,
+      })
+      .create({
+        _type: "comment",
+        name,
+        text,
+        post: {
+          _type: "reference",
+          _ref: _id,
+        },
+      });
 
-  return new Response(JSON.stringify({ message: "OK" }), {
-    status: 200,
-  });
+    return new Response(JSON.stringify({ message: "OK" }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 400,
+    });
+  }
 }
