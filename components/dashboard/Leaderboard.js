775671aa8e2f5679f67faa34626d7e3255c78826
Jesse Abuaja <jesseabuaja@MacBook-Pro.local>
2024-03-31 20:32:07 +0100

    Chore: Added tableau dashboard and leaderboard.

--- components/dashboard/Leaderboard.js
+++ components/dashboard/Leaderboard.js
@@ -14,84 +14,50 @@ export const Leaderboard = () => {
   }, [setTheme]);
 
   useEffect(() => {
-    var divElement = document.getElementById("viz1697709748497");
-    var vizElement = divElement.getElementsByTagName("object")[0];
-    if (divElement.offsetWidth > 800) {
-      vizElement.style.width = "100%";
-      vizElement.style.height = "1027px";
-    } else if (divElement.offsetWidth > 500) {
-      vizElement.style.width = "100%";
-      vizElement.style.height = "1027px";
-    } else {
-      vizElement.style.width = "100%";
-      vizElement.style.height = "1127px";
-    }
-    var scriptElement = document.createElement("script");
-    scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+    // Function to load Tableau script
+    const loadTableauScript = () => {
+      const script = document.createElement("script");
+      script.type = "module";
+      script.src =
+        "https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js";
+      script.async = true;
+      script.onload = () => {
+        // Script loaded successfully
+        console.log("Tableau script loaded.");
+        // Here you can write additional code that depends on the script being loaded
+      };
+      script.onerror = (error) => {
+        // Error occurred while loading script
+        console.error("Error loading Tableau script:", error);
+      };
+      document.body.appendChild(script);
+    };
+
+    // Call the function to load the script
+    loadTableauScript();
+
+    // Cleanup function
+    return () => {
+      // Remove the script from the DOM to prevent memory leaks
+      const tableauScript = document.querySelector(
+        '[src="https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js"]'
+      );
+      if (tableauScript) {
+        tableauScript.remove();
+      }
+    };
   }, []);
 
   return (
     <main
       id="view"
-      className="w-full bg-[#155F60] dark:bg-[#032929] mt-[30px] "
+      className="w-full bg-[#155F60] dark:bg-[#032929] mt-[30px] p-[5vmax] flex justify-center"
     >
-      <div className={"mx-auto"}>
-        <div style={{ position: "relative", padding: "5vmax" }}>
-          <div
-            style={{
-              position: "relative",
-            }}
-            className="mx-auto shadow-[inset_0_0_0_0.5vw_#155F60] max-w-[1200px_!important] dark:shadow-[inset_0_0_0_0.5vw_#032929]"
-          >
-            <div className="relative rounded-3xl overflow-hidden bg-white">
-              <div
-                id="viz1697709748497"
-                style={{
-                  position: "relative",
-                }}
-                className="tableauPlaceholder h-[1020px_!important] min-[519px]:h-[670px_!important] sm:rounded-[30px] rounded-[10px] overflow-[hidden_!important]"
-              >
-                <noscript>
-                  <a href="#">
-                    <img
-                      alt="Dashboard 1 "
-                      src="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Le&#47;Leaderboard_16975682382460&#47;Dashboard1&#47;1_rss.png"
-                      style={{ border: "none" }}
-                    />
-                  </a>
-                </noscript>
-                <object className="tableauViz" style={{ display: "none" }}>
-                  <param
-                    name="host_url"
-                    value="https%3A%2F%2Fpublic.tableau.com%2F"
-                  />
-                  <param name="embed_code_version" value="3" />
-                  <param name="site_root" value="" />
-                  <param
-                    name="name"
-                    value="Leaderboard_16975682382460&#47;Dashboard1"
-                  />
-                  <param name="tabs" value="no" />
-                  <param name="toolbar" value="yes" />
-                  <param
-                    name="static_image"
-                    value="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Le&#47;Leaderboard_16975682382460&#47;Dashboard1&#47;1.png"
-                  />
-                  <param name="animate_transition" value="yes" />
-                  <param name="display_static_image" value="yes" />
-                  <param name="display_spinner" value="yes" />
-                  <param name="display_overlay" value="yes" />
-                  <param name="display_count" value="yes" />
-                  <param name="language" value="en-US" />
-                  <param name="filter" value="publish=yes" />
-                </object>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div id="view"></div>
-      </div>
+      <tableau-viz
+        toolbar="hidden"
+        id="tableauViz"
+        src="https://public.tableau.com/views/Leaderboard_Kehinde/Top?:language=en-GB&:sid=&:display_count=n&:origin=viz_share_link"
+      ></tableau-viz>
     </main>
   );
 };
